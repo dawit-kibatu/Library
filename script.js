05f91722461fc3d666023fe077ce4b7e25299660
Dawit <208569702+dawit-kibatu@users.noreply.github.com.>
2026-02-10 12:10:17 +0300

    fetch: a compnent to create and store book

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const myLibrary=[];
-function book(name,author,pageNumber,read){
+function Book(name,author,pageNumber,read){
     this.name=name;
     this.author=author;
     this.pageNumber=pageNumber;
     this.read=read;
-    this.id=crypto.randomUUID()
+    this.id=crypto.randomUUID();
+}
+function addBookToLibrary(name,author,pageNumber,read){
+    let newBook=new book(name,author,pageNumber,read)
+    myLibrary.push(newBook);
 }
 
